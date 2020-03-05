addb010c785908940ea2773a86450fe9cd0601f9
jenbeltran <francescajenbeltran@gmail.com>
2020-03-05 16:41:27 -0500

    skills section completed

---
+++ src/components/Skills/data.js
@@ -0,0 +1,64 @@
+import html from '../../images/html-5.png';
+import css from '../../images/css.png';
+import javascript from '../../images/javascript.png';
+import react from '../../images/react.png';
+import node from '../../images/nodejs.png';
+import mysql from '../../images/mysql.png';
+import git from '../../images/git.png';
+import github from '../../images/github.png';
+import aws from '../../images/aws.png';
+import accessibility from '../../images/web-accessibility.png';
+
+const CodingStack = [
+	{
+		photo    : html,
+		alt      : 'HTML5 logo',
+		language : 'HTML 5'
+	},
+	{
+		photo    : css,
+		alt      : 'CSS logo',
+		language : 'CSS'
+	},
+	{
+		photo    : javascript,
+		alt      : 'JavaScript logo',
+		language : 'JavaScript'
+	},
+	{
+		photo    : react,
+		alt      : 'ReactJS logo',
+		language : 'ReactJS'
+	},
+	{
+		photo    : node,
+		alt      : 'NodeJS logo',
+		language : 'NodeJS'
+	},
+	{
+		photo    : mysql,
+		alt      : 'MySQL logo',
+		language : 'MySQL'
+	},
+	{
+		photo    : git,
+		alt      : 'Git logo',
+		language : 'Git'
+	},
+	{
+		photo    : github,
+		alt      : 'GitHub logo',
+		language : 'GitHub'
+	},
+	{
+		photo    : aws,
+		alt      : 'AWS logo',
+		language : 'AWS'
+	},
+	{
+		photo    : accessibility,
+		alt      : 'Web Accessibility logo',
+		language : 'Web Accessibility'
+	}
+];
+export default CodingStack;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,20 @@ import React from 'react';
 import SEO from '../components/seo';
 
 import AboutMe from '../components/AboutMe';
+
+//Section Titles
 import SectionTitle from '../components/SectionTitle';
 import { HighlightsTitle, SkillsTitle, HobbiesTitle } from '../components/SectionTitle/data';
+
+//Highlights Section
 import HighlightOne from '../components/CardImage';
 import HighlightTwo from '../components/CardImageTwo';
 import HighlightThree from '../components/CardImage';
 import HighlightFour from '../components/CardImageTwo';
 import Events from '../components/CardImage/data';
+
+//Skills Section
+import Skills from '../components/Skills';
 
 const IndexPage = () => (
 	<div>
@@ -42,6 +49,7 @@ const IndexPage = () => (
 			eventDescription={Events[3].eventDescription}
 		/>
 		<SectionTitle title={SkillsTitle.title} sentence={SkillsTitle.sentence} />
+		<Skills />
 		<SectionTitle title={HobbiesTitle.title} sentence={HobbiesTitle.sentence} />
 	</div>
 );
