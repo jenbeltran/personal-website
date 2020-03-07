625bbb198dc68bedc2a19eea16cc1341ebff00ab
jenbeltran <francescajenbeltran@gmail.com>
2020-03-07 17:29:40 -0500

    add buttons to projects

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { colors } from './colors';
 
-const { indigo } = colors;
+const { pink, indigo, white } = colors;
 
 export const ParagraphTitle = styled.p`
 	font-family: lato;
@@ -14,4 +14,12 @@ export const ParagraphDescription = styled.p`
 	font-family: lato;
 	font-size: 1.2rem;
 	line-height: 30px;
+`;
+
+export const Anchor = styled.a`text-decoration: none;`;
+
+export const Button = styled.button`
+	background: ${white};
+	color: ${pink};
+	border: 2px solid ${pink};
 `;
